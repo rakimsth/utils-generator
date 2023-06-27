8f27cdaf82bb34a40fe31b5722170924da445965
Raktim Shrestha <raktim@rumsan.com>
2023-06-27 14:38:52 +0545

    middleware, templating

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const ejs = require("ejs");
 
 const app = express();
 
 const indexRouter = require("./routes");
+
+// Setting up the third party middlewares
+app.use(morgan("short"));
+app.use(cors());
+
+// Setting up the EJS Templating
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.use("/", indexRouter);
 
--- routes/routes.ui.js
+++ routes/routes.ui.js
@@ -2,21 +2,34 @@ const express = require("express");
 const router = express.Router();
 const CC = require("currency-converter-lt");
 
+// const secureUI = (req, res, next) => {
+//   console.log("i am middleware");
+//   next();
+// };
+
 router.get("/", (req, res) => {
-  res.send("Hello from UI");
+  res.render("index");
 });
 
-router.get("/converter/:currency1/:currency2/:value", async (req, res) => {
-  const { currency1, currency2, value } = req.params;
-  const currencyConverter = new CC({
-    from: currency1,
-    to: currency2,
-    amount: Number(value),
-  });
-  const result = await currencyConverter.convert();
-  res.send(
-    `Currency Price of ${value} ${currency1} in ${currency2} is ${result}`
-  );
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
+
+router.get(
+  "/converter/:currency1/:currency2/:value",
+  // secureUI,
+  async (req, res) => {
+    const { currency1, currency2, value } = req.params;
+    const currencyConverter = new CC({
+      from: currency1,
+      to: currency2,
+      amount: Number(value),
+    });
+    const result = await currencyConverter.convert();
+    res.send(
+      `Currency Price of ${value} ${currency1} in ${currency2} is ${result}`
+    );
+  }
+);
 
 module.exports = router;
